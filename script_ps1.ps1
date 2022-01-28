ea64cc620a0ca6e08b2c6ecad29c0e36b7cbdb33
Benjamin Cote <70660430+becote@users.noreply.github.com>
2022-01-28 15:34:44 -0500

    foler profile

--- script_ps1.ps1
+++ script_ps1.ps1
@@ -1,7 +1,14 @@
+#Install git
+winget install -e --id Git.Git
 #Install oh-my-posh.exe and themes folder
 winget install JanDeDobbeleer.OhMyPosh
 #Update oh-my-posh
 winget upgrade JanDeDobbeleer.OhMyPosh
 
+
+#Create PowershellProfile
+New-Item -ItemType "directory" -Path "c:\ps-test\script\" -Force
+
+git clone 
 
 
